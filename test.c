093e3633ec559f54631bcb57d87ee9323e0a2922
Inigo Ibanez ibanez de aldecoa <iibanez-@c3r4s3.42urduliz.com>
2021-12-21 18:51:14 +0100

    lexer 1

--- test.c
+++ test.c
@@ -10,6 +10,48 @@ char	*readline(const char *prompt);
 Crear strcmp
 */
 
+int	ft_strlen(char *str)
+{
+	int	size;
+
+	size = 0;
+	while (str[size])
+		size = size + 1;
+	return (size);
+}
+
+char	*ft_strcpy(char *src)
+{
+	char	*dest;
+	int		i;
+
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+char	*ft_get_next_token(char *src)
+{
+	return (NULL)
+}
+
+char **ft_lexer(char *readl)
+{
+	char	*buff;
+	char	**result;
+	int		i;
+
+	buff = ft_strcpy(readl);
+
+	free(buff);
+	return (NULL);
+}
+
 int main()
 {
 	char	*readl;
@@ -21,6 +63,5 @@ int main()
 		printf( "%s\n", readl);
 		readl = readline( "test> ");
 	}
-	
     return 0;
 }

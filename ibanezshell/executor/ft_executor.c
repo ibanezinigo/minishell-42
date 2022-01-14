f56daa642176323c1d0f427cde6735c1de9a2df0
Inigo Gonzalez portela <ingonzal@c3r4s5.42urduliz.com>
2022-01-14 17:24:55 +0100

    satash c

--- ibanezshell/executor/ft_executor.c
+++ ibanezshell/executor/ft_executor.c
@@ -6,27 +6,33 @@
 /*   By: iibanez- <iibanez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 11:40:26 by iibanez-          #+#    #+#             */
-/*   Updated: 2022/01/13 16:02:25 by iibanez-         ###   ########.fr       */
+/*   Updated: 2022/01/13 19:58:06 by iibanez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-void	ft_execve(t_execution *exe, char *path, char **args)
+char	*ft_expand(char *token, int *i, t_list *envp);
+void	ft_clean_quote(t_list *command, t_list *envp);
+
+void	ft_execve(t_execution *exe, char *path, char **args, int z)
 {
 	char	**env;
-	int		wr;
+	//int		wr;
 
 	env = ft_listtotable(exe->envp2[0]);
-	if (exe->input != NULL)
+	/*if (exe->input != NULL)
 	{
+		printf("NO DEBE ENTRA\n");
 		dup2(exe->in[0], STDIN_FILENO);
 		wr = write(exe->in[1], exe->input, ft_strlen(exe->input));
-	}
-	dup2 (exe->out[1], STDOUT_FILENO);
-	close(exe->out[0]);
-	close(exe->out[1]);
+	}*/
+	dup2 (exe->in[0], STDIN_FILENO);
+	if (z == 0)
+		dup2 (exe->in[1], STDOUT_FILENO);
 	close(exe->in[0]);
+    //close(exe->out[1]);
+	//close(exe->out[0]);
 	close(exe->in[1]);
 	if (path)
 		execve(path, args, env);
@@ -34,7 +40,7 @@ void	ft_execve(t_execution *exe, char *path, char **args)
 	free(path);
 }
 
-void	ft_execute_fork(t_list *command, t_execution *exe)
+void	ft_execute_fork(t_list *command, t_execution *exe, int z)
 {
 	char	*path;
 	char	**args;
@@ -53,34 +59,94 @@ void	ft_execute_fork(t_list *command, t_execution *exe)
 		ft_free_list(env);
 	}
 	if (path != NULL)
-		ft_execve(exe, path, args);
+		ft_execve(exe, path, args, z);
+	/*
 	write(exe->out[1], "bash: ", 6);
 	write(exe->out[1], args[0], ft_strlen(args[0]));
-	write(exe->out[1], ": command not found\n", 20);
+	write(exe->out[1], ": command not found\n", 20);*/
 	ft_free_list(args);
 	exit(127);
 }
 
-void	ft_execute_not_builtin(t_list *command, t_execution *exe)
+void	ft_execute_not_builtin(t_list *command, t_execution *exe, int z)
 {
 	pid_t	pid;
 
-	pipe(exe->out);
-	pipe(exe->in);
+//int fd;
+//dup(fd, STDIN_FILENO);
+//pipe(exec->in)
 	pid = fork();
 	if (pid == 0)
-		ft_execute_fork(command, exe);
+		ft_execute_fork(command, exe, z);
+		//dup2(exec-in[0], fd);
 	else
 	{
-		g_global.pid = pid;
 		close(exe->out[1]);
-		close(exe->in[0]);
-		close(exe->in[1]);
-		wait(&pid);
+		g_global.pid = pid;
 		g_global.errnor = WEXITSTATUS(pid);
-		ft_read_execve(exe);
-		close(exe->out[0]);
+		//ft_read_execve(exe);
 	}
+}
+
+
+
+void	ft_execute_aux(t_list *command, t_execution *exe, int z)
+{
+	ft_str_to_lower(command->token);
+	ft_clean_quote(command, exe->envp2[0]);
+	if (ft_strequals(command->token, "echo"))
+		ft_echo(command, exe);
+	else if (ft_strequals(command->token, "cd"))
+		ft_cd(command, exe);
+	else if (ft_strequals(command->token, "pwd"))
+		ft_pwd(exe);
+	else if (ft_strequals(command->token, "export"))
+		ft_export(command, exe);
+	else if (ft_strequals(command->token, "unset"))
+		ft_unset(command, exe);
+	else if (ft_strequals(command->token, "env"))
+		ft_env(exe);
+	else if (ft_strequals(command->token, "exit"))
+		ft_exit(command);
+	else
+		ft_execute_not_builtin(command, exe, z);
+	if (exe->input)
+	{
+		free(exe->input);
+		exe->input = NULL;
+	}
+}
+
+#include <stdio.h>
+int	ft_execute(t_list **commands, t_execution *exe)
+{
+	int	i;
+
+	i = 0;
+	exe->output = NULL;
+	exe->input = NULL;
+	exe->error = NULL;
+	exe->redi = 0;
+	pipe(exe->in);
+	while (commands[i] != NULL)
+	{
+		commands[i] = ft_check_input(commands[i], exe);
+		commands[i] = ft_check_output(commands[i], exe);
+		pipe(exe->out);
+		if (!(exe->in_redi != -1 && exe->input == NULL))
+		{
+			if (commands[i + 1] == NULL)
+				ft_execute_aux(commands[i], exe, 1);
+			else
+				ft_execute_aux(commands[i], exe, 0);
+		}
+		if (exe->redi == 1 || exe->redi == 2)
+			free(exe->outputfile);
+		i++;
+		exe->in[1] = dup2(exe->out[0], 1);
+	}
+	//ft_print_output(exe);
+	return (0);
 }
 
 char	*ft_expand(char *token, int *i, t_list *envp)
@@ -152,61 +218,4 @@ void	ft_clean_quote(t_list *command, t_list *envp)
 		free(str);
 		next = next->next;
 	}
-}
-
-void	ft_execute_aux(t_list *command, t_execution *exe)
-{
-	ft_str_to_lower(command->token);
-	ft_clean_quote(command, exe->envp2[0]);
-	if (ft_strequals(command->token, "echo"))
-		ft_echo(command, exe);
-	else if (ft_strequals(command->token, "cd"))
-		ft_cd(command, exe);
-	else if (ft_strequals(command->token, "pwd"))
-		ft_pwd(exe);
-	else if (ft_strequals(command->token, "export"))
-		ft_export(command, exe);
-	else if (ft_strequals(command->token, "unset"))
-		ft_unset(command, exe);
-	else if (ft_strequals(command->token, "env"))
-		ft_env(exe);
-	else if (ft_strequals(command->token, "exit"))
-		ft_exit(command);
-	else
-		ft_execute_not_builtin(command, exe);
-	if (exe->input)
-	{
-		free(exe->input);
-		exe->input = NULL;
-	}
-}
-
-#include <stdio.h>
-int	ft_execute(t_list **commands, t_execution *exe)
-{
-	int	i;
-
-	i = 0;
-	exe->output = NULL;
-	exe->input = NULL;
-	exe->error = NULL;
-	exe->redi = 0;
-	while (commands[i] != NULL)
-	{
-		commands[i] = ft_check_input(commands[i], exe);
-		commands[i] = ft_check_output(commands[i], exe);
-		if (!(exe->in_redi != -1 && exe->input == NULL))
-		{
-			if (commands[i] != NULL)
-			{
-				ft_execute_aux(commands[i], exe);
-				ft_redirect_output(exe);
-			}
-		}
-		if (exe->redi == 1 || exe->redi == 2)
-			free(exe->outputfile);
-		i++;
-	}
-	ft_print_output(exe);
-	return (0);
 }
